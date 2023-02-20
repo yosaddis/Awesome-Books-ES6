abafbe54dde81438a6921cbe4861b1068091c589
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-21 01:13:16 +0300

    Modify the module to use luxon

--- js/modules/datetime.js
+++ js/modules/datetime.js
@@ -1,39 +1,39 @@
-import { DateTime } from "./luxon/luxon.js";
+/* eslint-disable import/prefer-default-export */
+import { DateTime } from './luxon/luxon.js';
 
 // create a date-time function
 export const CurrentDateTime = () => {
+  const dateDisplay = document.querySelector('.header__date');
+  const dateObject = DateTime.now();
+  const yearNow = dateObject.toFormat('yyyy');
+  const monthNow = dateObject.toFormat('MMMM');
+  const dayNow = parseInt(dateObject.toFormat('d'), 10);
+  const timeNow = dateObject.toFormat('h:m:s a');
 
-	const dateDisplay = document.querySelector('.header__date');
-	const dateObject = new Date();
-	const yearNow = dateObject.getFullYear();
-	const monthNow = dateObject.toLocaleString('default', { month: 'long' });
-	const dayNow = dateObject.getDate();
-	const timeNow = dateObject.toLocaleTimeString();
-
-	// Append suffix to day
-	if (dayNow > 3 && dayNow < 21) {
-		const date = `${monthNow} ${dayNow}th ${yearNow}`;
-		dateDisplay.innerHTML = `${date}, ${timeNow}`;
-	} else {
-		const d = dayNow % 10;
-		let date = '';
-		switch (d) {
-			case 1:
-				date = `${monthNow} ${dayNow}st ${yearNow}`;
-				dateDisplay.innerHTML = `${date}, ${timeNow}`;
-				break;
-			case 2:
-				date = `${monthNow} ${dayNow}nd ${yearNow}`;
-				dateDisplay.innerHTML = `${date}, ${timeNow}`;
-				break;
-			case 3:
-				date = `${monthNow} ${dayNow}rd ${yearNow}`;
-				dateDisplay.innerHTML = `${date}, ${timeNow}`;
-				break;
-			default:
-				date = `${monthNow} ${dayNow}th ${yearNow}`;
-				dateDisplay.innerHTML = `${date}, ${timeNow}`;
-				break;
-		}
-	}
+  // Append suffix to day
+  if (dayNow > 3 && dayNow < 21) {
+    const date = `${monthNow} ${dayNow}th ${yearNow}`;
+    dateDisplay.innerHTML = `${date}, ${timeNow}`;
+  } else {
+    const d = dayNow % 10;
+    let date = '';
+    switch (d) {
+      case 1:
+        date = `${monthNow} ${dayNow}st ${yearNow}`;
+        dateDisplay.innerHTML = `${date}, ${timeNow}`;
+        break;
+      case 2:
+        date = `${monthNow} ${dayNow}nd ${yearNow}`;
+        dateDisplay.innerHTML = `${date}, ${timeNow}`;
+        break;
+      case 3:
+        date = `${monthNow} ${dayNow}rd ${yearNow}`;
+        dateDisplay.innerHTML = `${date}, ${timeNow}`;
+        break;
+      default:
+        date = `${monthNow} ${dayNow}th ${yearNow}`;
+        dateDisplay.innerHTML = `${date}, ${timeNow}`;
+        break;
+    }
+  }
 };
