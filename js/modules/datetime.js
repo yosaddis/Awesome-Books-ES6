70b9af549186f591d8aa1362a73058ffc01a6f29
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-21 00:41:16 +0300

    Move datetime function to module

---
+++ js/modules/datetime.js
@@ -0,0 +1,39 @@
+//import { DateTime } from "./luxon/luxon";
+
+// create a date-time function
+export const CurrentDateTime = () => {
+
+	const dateDisplay = document.querySelector('.header__date');
+	const dateObject = new Date();
+	const yearNow = dateObject.getFullYear();
+	const monthNow = dateObject.toLocaleString('default', { month: 'long' });
+	const dayNow = dateObject.getDate();
+	const timeNow = dateObject.toLocaleTimeString();
+
+	// Append suffix to day
+	if (dayNow > 3 && dayNow < 21) {
+		const date = `${monthNow} ${dayNow}th ${yearNow}`;
+		dateDisplay.innerHTML = `${date}, ${timeNow}`;
+	} else {
+		const d = dayNow % 10;
+		let date = '';
+		switch (d) {
+			case 1:
+				date = `${monthNow} ${dayNow}st ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+			case 2:
+				date = `${monthNow} ${dayNow}nd ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+			case 3:
+				date = `${monthNow} ${dayNow}rd ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+			default:
+				date = `${monthNow} ${dayNow}th ${yearNow}`;
+				dateDisplay.innerHTML = `${date}, ${timeNow}`;
+				break;
+		}
+	}
+};
